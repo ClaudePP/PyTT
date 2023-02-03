57a9e6bf79dd8e816b3a36458d6ef54eac6b803b
Araceli Navarro <54847307+navarrof@users.noreply.github.com>
2023-02-03 16:24:47 +0100

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -11,13 +11,22 @@ from Modules import LoadingFileFunctions
 from Modules import CoreSimulationFunctions
 from Modules import NecessaryVariables as nv
 
+# Set up necessary paths. 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 nv.ParticleInfo_dir = resource_path("ParticleInfo")
-# -------- Simulation with Userfriendly interface --------- #
+
+
+# The main function checks how many arguments the user gave when executing the code: 
+# if python MAIN.py : Then only 1 argument was given. 
+# If Python MAIN.py InputFile.txt: Then two arguments were given. 
+# 
+# In the case of giving no imput file the program calls the user friendly interface. 
+# In case there was an inputfile, the MAIN_CALLSIMULATION is directly called. 
+
 
 def main():
 
@@ -35,7 +44,7 @@ def main():
         sys.exit(app.exec_())
         
         
-
+# The code starts here, when the user executes this MAIN.py file. 
 
 if __name__ == '__main__':
     main()
